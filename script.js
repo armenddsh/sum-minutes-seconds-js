0af3b28eb6c723b320a01c6b666891081d4ad551
Armend Shala <79515966+armenddsh@users.noreply.github.com>
2022-03-23 22:35:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,25 +2,25 @@ var totalH;
 var totalM;
 
 const times = [
-  "(08:15)",
-  "(08:45)",
-  "(08:45)"
+ 
 ]
 
 function sumTimes(times) {
   let totalM = 0;
   let totalS = 0;
   let totalH = 0;
-
+  let totalD = 0;
+  
   for(let time of times) {
       let t = time.substring(1, time.length - 1);
       let tSplited = t.split(":");
-      console.log(tSplited);
+
       const minutes = parseInt(tSplited[0], 10);
       const seconds = parseInt(tSplited[1], 10);
     
       totalM += minutes;
       totalS += seconds;
+    
       if (totalS >= 60) {
         totalS = totalS - 60;
         totalM += 1;
@@ -29,8 +29,12 @@ function sumTimes(times) {
         totalM = totalM - 60;
         totalH += 1;    
       }
+      if (totalH >= 24) {
+        totalH = totalH - 24;
+        totalD += 1;
+      }
   }
-  return `${totalH} hours ${totalM} minutes ${totalS} seconds`;
+  return `${totalD} days ${totalH} hours ${totalM} minutes ${totalS} seconds`;
 }
 
 console.log(sumTimes(times));
